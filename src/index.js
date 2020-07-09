0496beff646d1681787c24012072bb8506c06133
Diego Argueta <dargueta1879@gmail.com>
2020-07-09 13:43:25 -0600

    Se agregó componente global ChatProvider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ChatProvider from './components/context/ChatProvider';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChatProvider>
     <App />
+    </ChatProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
